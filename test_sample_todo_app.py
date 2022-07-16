e26ed098fac71bbb3e4413926e4b5e1226ba33b0
Ken Goddard <closerwalk@gmail.com>
2022-07-15 17:55:20 -0700

    Update test_sample_todo_app.py

--- test_sample_todo_app.py
+++ test_sample_todo_app.py
@@ -9,7 +9,7 @@ capabilities = {
     "name" : "Quick Test",
     "platform" : "Windows 10",
     "browserName" : "Chrome",
-    "version" : "93.0",
+    "version" : "82.0",
     "resolution" : "1920x1080",
     "tunnel" : True,
     "geoLocation" : "US"
